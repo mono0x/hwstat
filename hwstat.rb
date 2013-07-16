6f8860fcef0f926a9386080535b20b5319e0779e
Yusuke Yasui <ysk.univ.1007@gmail.com>
2013-07-16 14:45:18 +0900

    first

---
+++ hwstat.rb
@@ -0,0 +1,73 @@
+#!/usr/bin/ruby
+
+STDOUT.sync = true
+
+NETDEV_NAME = ARGV[0]
+INTERVAL = 1
+NR_CORES = `cat /proc/cpuinfo | grep '^processor' | wc -l`.to_i
+
+class Integer
+    def diff(given)
+        self - given
+    end
+end
+
+class Array
+    def diff(given)
+        Array.new(self.length) { |i|
+            self[i].diff(given[i])
+        }
+    end
+
+    def to_i
+        self.map { |x| x.to_i }
+    end
+end
+
+class Hash
+    def diff(given)
+        self.keys.inject({}) { |ret, key|
+            ret[key] = self[key].diff(given[key])
+            ret
+        }
+    end
+end
+
+def get_hwstat
+    hwstat = {}
+    hwstat[:cpu] = open('/proc/stat') { |f|
+        f.to_a[1..NR_CORES].map { |line|
+            Hash[[:user, :nice, :system, :irq, :softirq].zip(line.split(' ').values_at(1, 2, 3, 6, 7).to_i)]
+        }
+    }
+    hwstat[:netsnmp] = open('/proc/net/snmp') { |f|
+        Hash[[:InSegs, :OutSegs, :RetransSegs].zip(f.select { |line| line.include?('Tcp:') }[1].split(' ').values_at(10, 11, 12).to_i)]
+    }
+    if NETDEV_NAME != nil
+        hwstat[:netdev] = open('/proc/net/dev') { |f|
+            items = [:bytes, :packets, :drop]
+            tmp = f.find { |line| line.include?("#{NETDEV_NAME}") }.split(' ').values_at(1, 2, 4, 9, 10, 12).to_i
+            {
+                receive:
+                Hash[items.zip(tmp[0, 3])],
+                transmit:
+                Hash[items.zip(tmp[3, 3])],
+            }
+        }
+        hwstat[:interrupts] = open('/proc/interrupts') { |f|
+            f.select { |line| line.include?("#{NETDEV_NAME}") }.inject({}) { |h, line|
+                tmp = line.split(' ')
+                h[tmp[NR_CORES + 2]] = tmp[1, NR_CORES].to_i
+                h
+            }
+        }
+    end
+    hwstat
+end
+
+crr = get_hwstat
+while true
+    sleep INTERVAL
+    prv, crr = crr, get_hwstat
+    p crr.diff(prv)
+end
---
+++ hwstat_curses.rb
@@ -0,0 +1,67 @@
+#!/usr/bin/ruby
+
+require 'curses'
+
+TABLE_COLUMN_WIDTH = 12
+
+Curses.init_screen
+Curses.noecho
+Curses.curs_set(0)
+Curses.start_color
+Curses.init_pair(1, Curses::COLOR_WHITE, Curses::COLOR_BLACK)
+Curses.init_pair(2, Curses::COLOR_GREEN, Curses::COLOR_BLACK)
+Curses.init_pair(3, Curses::COLOR_CYAN, Curses::COLOR_BLACK)
+Curses.init_pair(4, Curses::COLOR_RED, Curses::COLOR_BLACK)
+Curses.init_pair(5, Curses::COLOR_MAGENTA, Curses::COLOR_BLACK)
+Curses.init_pair(6, Curses::COLOR_YELLOW, Curses::COLOR_BLACK)
+
+while true
+    hwstat = eval Curses.getstr
+    Curses.clear
+    Curses.setpos(0, 0)
+
+    Curses.attron(Curses.color_pair(1))
+    Curses.addstr("[CPU]\n")
+    Curses.attroff(Curses::A_COLOR)
+    hwstat[:cpu].each_with_index { |percpu, id|
+        Curses.attron(Curses.color_pair(1))
+        Curses.addstr("cpu#{id}: #{sprintf('%3d', percpu.each_value.inject(0) { |s, x| s + x })} ")
+        Curses.attroff(Curses::A_COLOR)
+        [:user, :nice, :system, :irq, :softirq].each_with_index { |key, i|
+            Curses.attron(Curses.color_pair(i + 2))
+            Curses.addstr('|' * percpu[key])
+            Curses.attroff(Curses::A_COLOR)
+        }
+        Curses.addstr("\n")
+    }
+
+    Curses.attron(Curses.color_pair(1))
+    Curses.addstr("\n" <<
+                  "[TCP]\n" <<
+                  hwstat[:netsnmp].inject('') { |s, (k, v)| s << sprintf("%#{TABLE_COLUMN_WIDTH}s", k) } << "\n" <<
+                  '-' * TABLE_COLUMN_WIDTH * hwstat[:netsnmp].length << "\n" <<
+                  hwstat[:netsnmp].inject('') { |s, (k, v)| s << sprintf("%#{TABLE_COLUMN_WIDTH}s", v) } << "\n" <<
+                  "\n" <<
+                  "[Network]\n" <<
+                  '        type |         Mb/s' <<
+                  (hwstat[:netdev][:receive].keys - [:bytes]).inject('') { |s, item| s << sprintf("%#{TABLE_COLUMN_WIDTH}s", item) } << "\n" <<
+                  '-' * (3 + TABLE_COLUMN_WIDTH * (hwstat[:netdev][:receive].keys.length + 1)) << "\n" <<
+                  hwstat[:netdev].inject('') { |s, (k, v)|
+                      s << sprintf("%#{TABLE_COLUMN_WIDTH}s", k) << " | " <<
+                      sprintf("%#{TABLE_COLUMN_WIDTH}.3f", v[:bytes] * 8 / 1000000.0) <<
+                      (v.keys - [:bytes]).inject('') { |s, item| s << sprintf("%#{TABLE_COLUMN_WIDTH}d", v[item]) } << "\n"
+                  } <<
+                  "\n" <<
+                  "[Interrupts]\n" <<
+                  '        name | ' << hwstat[:cpu].length.times.inject('') { |s, i| s << "        cpu#{i}" } << "\n" <<
+                  '-' * (15 + 12 * hwstat[:cpu].length) << "\n" <<
+                  hwstat[:interrupts].inject('') { |s, (k, v)|
+                      s << sprintf("%#{TABLE_COLUMN_WIDTH}s", k) << " | " <<
+                      v.inject('') { |s, x| s << sprintf("%#{TABLE_COLUMN_WIDTH}d", x) } << "\n"
+                  })
+    Curses.attroff(Curses::A_COLOR)
+
+    Curses.refresh
+end
+
+Curses.close_screen
